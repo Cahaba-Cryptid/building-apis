1821e9ef816100daa4089338a676f18fbcf08ed2
Hayden <chfrank9011@gmail.com>
2019-04-25 06:20:52 -0500

    fixed some small bootstrap issues

--- client/app.js
+++ client/app.js
@@ -1,10 +1,26 @@
+const displayChirps = chirps => {
+    for (let key in chirps) {
+        $('#chirps').append(`
+        <article class="col-8-md">
+            <div class="card m-1 p-1 shadow">
+                <div class="card-body">
+                    <h3 class="card-title">${chirps[key].User}</h3>
+                    <p class="card-text">${chirps[key].chirp}
+                </div>
+            </div>
+        </article>
+        `)
+    }
+}
+
 const getChirps = async () => {
-    try { 
+    try {
         let chirps = await $.ajax({
-        type: 'GET',
-        url: '/api/chirps',
-    })
-        console.log(chirps)
+            type: 'GET',
+            url: '/api/chirps',
+        });
+        delete chirps['nextid'];
+        displayChirps(chirps)
     } catch (error) {
         console.log(error);
     }
