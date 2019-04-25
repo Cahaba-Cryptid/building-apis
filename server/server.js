17dd7eda6f7b17efdefc22e2f9550c8701399ab9
Hayden <chfrank9011@gmail.com>
2019-04-25 08:10:53 -0500

    new chirps appedning to body with inputs with refresh

--- server/server.js
+++ server/server.js
@@ -5,12 +5,10 @@ const apiRouter = require('./routes')
 
 const app = express();
 
-
 app.use(express.static('client'));
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
 
 app.use('/api', apiRouter);
 
